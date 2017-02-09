7a9fed596f4913ca890272a4e3d3f3e24d743592
Nima-Javaheri <nima.hjavaheri@gmail.com>
2017-02-09 20:01:21 +0000

    pythagonean

---
+++ pythagorean.js
@@ -0,0 +1,17 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+
+var result = input.map(function pythagor(theObject) {
+var sqrtOf = Math.sqrt ( Math.pow (theObject["x"],2)+ Math.pow (theObject["y"],2));
+return sqrtOf;
+
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
+console.log(result)
